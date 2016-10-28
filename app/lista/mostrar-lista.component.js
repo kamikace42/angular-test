5967cabb1ed43a022797778293dd38cfd2a7c834
kamikace <kamikace42@gmail.com>
2016-10-28 18:07:16 +0200

    json xhr

--- app/lista/mostrar-lista.component.js
+++ app/lista/mostrar-lista.component.js
@@ -4,13 +4,14 @@ component("mostrarLista", {
     templateUrl: 'app/lista/mostrar-lista.template.html',
     controller: function TestAppController() {
         this.lista = [{
-            nombre: "prueba1",
-            text: "me cago en to angular"
+            nombre: "aprueba1",
+            text: "bme cago en to angular"
         }, {
-            nombre: "prueba2",
-            text: "puta mierda"
+            nombre: "xprueba2",
+            text: "aputa mierda"
 
         }];
+        this.orden = "text";
     }
 
 });
